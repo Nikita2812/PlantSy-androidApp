9b204144127fb374c7601db780a71636dcf0892b
Nikita <prasadnikita611@gmail.com>
2024-08-28 17:04:49 +0530

    style: added theme colours

--- app/src/main/java/com/nikita_prasad/plantsy/ui/theme/Theme.kt
+++ app/src/main/java/com/nikita_prasad/plantsy/ui/theme/Theme.kt
@@ -9,19 +9,19 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = customGreen,
+    secondary = Color.Black,
+    tertiary = customYellow
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
+    primary = customGreen,
+    secondary = Color.White,
+    tertiary = customYellow
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
